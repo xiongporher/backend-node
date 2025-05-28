2acce857284a6f14844eed3e8f96fa5edcf188a3
Xinogpor <xiongporher1@gmail.com>
2025-05-28 23:37:22 +0700

    test code

--- route/index.route.js
+++ route/index.route.js
@@ -5,7 +5,7 @@ import { login, register } from '../controller/auth.controller.js';
 
 const route = express.Router();
 
-// route user 2000
+// route user 2000 !!!
 route.post("/user/create", register);
 route.post("/login", login);
 
